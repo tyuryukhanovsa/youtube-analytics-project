932b69a54192172bc94ff10c52febbc9851936d4
Sergey <serega.tiuriuhanov94@gmail.com>
2023-06-25 15:25:09 +0300

    added file pyproject.toml updated code in channel.py

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,6 @@
+from googleapiclient.discovery import build
+import os
+import json
 
 
 class Channel:
@@ -5,8 +8,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        dict_to_print = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
